81b456b88201bb7c1589e3402fc11618d085793e
Charles Butler <cdbutle@outlook.com>
2020-05-03 00:58:23 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -8,7 +8,7 @@ attr_accessor :name, :weight, :height
     @name = params[:name]
     @weight = params[:weight]
     @height = params[:height]
-    @@piratess << self
+    @@pirates << self
   end
 
   def self.all
