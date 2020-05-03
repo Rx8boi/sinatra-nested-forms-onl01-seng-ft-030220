6a5fb10a778fbae25df28b5d49cd74816a1df82b
Charles Butler <cdbutle@outlook.com>
2020-05-03 01:10:28 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -7,7 +7,7 @@ attr_accessor :name, :type, :booty_attributes
   def initialize(args)
     @name = args[:name]
     @type = args[:type]
-    @booty_attributes = args[:booty_attributes]
+    @booty = args[:booty_attributes]
     @@ships << self
   end
 
