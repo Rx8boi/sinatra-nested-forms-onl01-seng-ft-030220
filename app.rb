b4807eab63617c6990181a7a3904ef8e6af6287f
Charles Butler <cdbutle@outlook.com>
2020-05-03 00:54:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,11 +17,11 @@ module FormsLab
         params["pirate"]["ships"].each do |ship_data|
           Ship.new(ship_data["name"], ship_data["type"], ship_data["booty"])
         end
-  
+
         @pirate = Pirate.new(params["pirate"]["name"], params["pirate"]["height"], params["pirate"]["weight"])
-  
+
         @ships = Ship.all
-  
+
         erb :'pirates/show'
 
   end
